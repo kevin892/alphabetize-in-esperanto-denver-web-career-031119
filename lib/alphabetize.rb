80cea8625dd34dc36651410da77a4251a5339c8d
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 17:34:36 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
-def alphabetize(phrase)
+def alphabetize(sentence_array)
   alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
-  phrase.sort_by do |word|
-    word.split('').m do |letter|
+  sentence_array.sort_by do |sentence|
+    sentence.split('').map do |letter|
       alphabet.index(letter)
     end
   end
