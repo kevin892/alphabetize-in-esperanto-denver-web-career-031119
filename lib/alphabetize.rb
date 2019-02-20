c71c562827aa4ade740b477643216802aeff8546
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 17:34:55 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,15 @@
-def alphabetize(sentence_array)
-  alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
-  sentence_array.sort_by do |sentence|
-    sentence.split('').map do |letter|
-      alphabet.index(letter)
+# def alphabetize(sentence_array)
+#   alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+#   sentence_array.sort_by do |sentence|
+#     sentence.split('').map do |letter|
+#       alphabet.index(letter)
+#     end
+#   end
+# end
+def alphabetize(sentences_array)
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
     end
   end
 end
