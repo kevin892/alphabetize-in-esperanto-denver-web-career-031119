06ea311f1aad4b2739976fde5fb8657f41a7743d
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 17:29:32 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(phrase)
   alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
-  phrase.sort_by do |word|
+  phrase.sort do |word|
     word.split('').map do |letter|
       alphabet.index(letter)
     end
